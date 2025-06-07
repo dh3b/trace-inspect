babdcb35b03d8c52bc05ff439c749987653452d7
Mateusz Wieczorek <ozootly@gmail.com>
2025-06-07 20:07:23 +0200

    Experimenting with tracer

---
+++ tokenizer.py
@@ -0,0 +1,20 @@
+from sys import settrace
+
+def tracer(frame, event, arg = None):
+    code = frame.f_code
+    func_name = code.co_name
+    line_no = frame.f_lineno
+
+    print(f"A {event} encountered in \
+    {func_name}() at line number {line_no} ")
+
+    return tracer
+
+def foo():
+    return "foo_result"
+
+def check():
+    return foo()
+
+settrace(tracer)
+check()
