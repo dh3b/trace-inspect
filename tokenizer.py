9da717085bd74bb26e08837ef91221f59f060644
Mateusz Wieczorek <ozootly@gmail.com>
2025-06-08 17:53:35 +0200

    Basic variables changes tracking

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,8 @@
 from sys import settrace
+from collections import defaultdict
 
 INPUT_PATH = 'input.py'
+tokens = defaultdict(list)
 
 def tracer(frame, event, arg = None):
     depth = 0
@@ -13,11 +15,14 @@ def tracer(frame, event, arg = None):
         func_name = code.co_name
         line_no = frame.f_lineno
 
+        l_vars = frame.f_locals.copy()
+        for key, val in l_vars.items():
+            if not tokens[key] or tokens[key][-1] != val:
+                tokens[key].append(val)
+
         if event == 'line':
-            l_vars = frame.f_locals.copy()
             print(f"Line {line_no} → {l_vars}")
         elif event == 'return':
-            l_vars = frame.f_locals.copy()
             print(f"Return from {func_name}() → {l_vars} at line no. {line_no}")
         else:
             print(f"A {event} encountered in {func_name}() at line no. {line_no}")
@@ -37,6 +42,7 @@ def main():
     settrace(tracer)
     exec(x)
     settrace(None)
+    print(tokens)
 
 if __name__ == '__main__':
     main()
